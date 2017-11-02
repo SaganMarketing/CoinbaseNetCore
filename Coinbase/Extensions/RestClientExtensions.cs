917dd6921fbe35955c83281b39f3e5820c3eedf0
Sergey Sagan <support@saganmarketing.com>
2017-11-02 01:44:55 -0600

    Initial Commit

---
+++ Coinbase/Extensions/RestClientExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coinbase.Extensions
+{
+    public static class RestClientExtensions
+    {
+        //public static async Task<RestResponse> ExecuteAsync<T>(this IRestClient client, IRestRequest request)
+        //{
+        //    TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+        //    RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+        //    return (RestResponse)(await taskCompletion.Task);
+        //}
+        //public static RestResponse Execute<T>(this IRestClient client, IRestRequest request)
+        //{
+        //    TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+        //    RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+        //    return (RestResponse)(taskCompletion.Task.Result);
+        //}
+
+        public static T Execute<T>(this IRestClient client, IRestRequest request)
+        {
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            var content = taskCompletion.Task.Result.Content;
+
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
